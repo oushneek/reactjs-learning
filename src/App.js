16be19ac9d825a09b7c1f305aef08e41a09f5793
Oushneek <oushneek@gmail.com>
2019-12-06 13:24:21 +0600

    38 done

--- src/App.js
+++ src/App.js
@@ -3,17 +3,26 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <h1>Hi, I'm a React App.</h1>
-          <p>This is really working.!!!</p>
-          <Person name="Tazbeea" age="25"/>
-          <Person name="Tazakka" age="27">My Hobbies: Reading</Person>
-          <Person name="Oushneek" age="29"/>
+    state ={
+        persons: [
+            {name: 'Tazbeea', age: 25},
+            {name: 'Tazakka', age: 27},
+            {name: 'Oushneek', age: 29}
+        ]
+    }
 
-      </div>
-    );
+    render() {
+        return (
+          <div className="App">
+              <h1>Hi, I'm a React App.</h1>
+              <p>This is really working.!!!</p>
+              <button>Switch Name</button>
+              <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+              <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Reading</Person>
+              <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+
+          </div>
+        );
     //   return React.createElement('div',{className:'App'},React.createElement('h1',null,'Does this work now???'));
   }
 }

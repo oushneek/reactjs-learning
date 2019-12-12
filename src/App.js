28d0811e1923864abcabc6eb22ac3d8f9ec4bb90
Oushneek <oushneek@gmail.com>
2019-12-12 19:43:09 +0600

    70 incomplete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import Radium,{StyleRoot} from 'radium';
 import Person from './Person/Person';
 
 
@@ -89,7 +88,6 @@ class App extends Component {
             classes.push('bold');
         }
         return (
-            <StyleRoot>
                 <div className="App">
                     <h1>Hi, I'm a React App.</h1>
                     <p className={classes.join(' ')}>This is really working.!!!</p>
@@ -98,9 +96,9 @@ class App extends Component {
                         onClick={this.togglePersonsHandler}>Toggle Persons</button>
                     {persons}
                 </div>
-            </StyleRoot>
+
         );
         //   return React.createElement('div',{className:'App'},React.createElement('h1',null,'Does this work now???'));
     }
 }
-export default Radium(App);
+export default App;
